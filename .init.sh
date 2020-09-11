ee008f8ca8937424dcb8b149c634e3b9c3cb7f6f
eighty20results <thomas+github@eighty20results.com>
2020-09-11 18:51:54 +0000

    BUG FIX: Didn't import the clean database

--- .init.sh
+++ .init.sh
@@ -1,4 +1,5 @@
+wp db import ~/public_html/wp-content/plugins/${REPO_NAME}/tests/database/default_db.sql
 wp plugin delete hello
 wp plugin delete akismet
-wp plugin install paid-memberships-pro
+wp plugin install paid-memberships-pro --activate
 wp plugin activate ${REPO_NAME}
