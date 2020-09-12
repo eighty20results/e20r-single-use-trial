ba3b50fa7ac6676b0fda504cb25622d859962d7a
eighty20results <thomas+github@eighty20results.com>
2020-09-12 16:50:11 +0000

    BUG FIX: Not loading test DB during init

--- .init.sh
+++ .init.sh
@@ -1,5 +1,5 @@
 ln -s "/workspace/${REPO_NAME}/public_html/wp-content/plugins/${REPO_NAME}" "/workspace/${REPO_NAME}"
-wp db import "/workspace/${REPO_NAME}/${REPO_NAME}/tests/database/default_db.sql"
+wp db import "/workspace/${REPO_NAME}/${REPO_NAME}/tests/database/default_db.sql" db import "/workspace/${REPO_NAME}/${REPO_NAME}/tests/database/default_db.sql"
 wp plugin delete hello
 wp plugin delete akismet
 wp plugin install paid-memberships-pro --activate
