0990144ea443c97c1742a440bcc0f9d5218d0682
eighty20results <thomas+github@eighty20results.com>
2020-09-12 14:38:19 +0000

    BUG FIX: Fix pathing issues for DB import, etc.

--- .init.sh
+++ .init.sh
@@ -1,5 +1,7 @@
-wp db import ~/public_html/wp-content/plugins/${REPO_NAME}/tests/database/default_db.sql
+ln -s "/workspace/${REPO_NAME}/public_html/wp-content/plugins/${REPO_NAME}" "/workspace/${REPO_NAME}"
+wp db import "/workspace/${REPO_NAME}/${REPO_NAME}/tests/database/default_db.sql"
 wp plugin delete hello
 wp plugin delete akismet
 wp plugin install paid-memberships-pro --activate
 wp plugin activate ${REPO_NAME}
+
