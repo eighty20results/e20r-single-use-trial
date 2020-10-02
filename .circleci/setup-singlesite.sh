4c18788a1ddb9284433c4ee243c52eb5201d3d4c
Thomas Sjolshagen <thomas@eighty20results.com>
2020-10-02 09:27:26 -0600

    Various updates to try and get the wp install going

--- .circleci/setup-singlesite.sh
+++ .circleci/setup-singlesite.sh
@@ -20,46 +20,71 @@ if [[ -z "$WP_ORG_PLUGIN_NAME" ]]; then
     exit 1
 fi
 
+home_dir="${HOME}/${WP_ORG_PLUGIN_NAME}"
+
+echo "---------------------------------------------------------------"
+ls -la "${home_dir}/inc/bin/"
+echo "---------------------------------------------------------------"
+
+# Load WordPress and install into core directory
+echo "Create WordPress install directory (${WP_CORE_DIR}), cd to it and download WordPress."
+mkdir -p "${WP_CORE_DIR}"
+cd "${WP_CORE_DIR}"
+
+"${home_dir}/inc/bin/wp" core download
+
+echo "Create WordPress config files in ${WP_CORE_DIR}"
+echo "${home_dir}/inc/bin/wp config create \
+      	--path=${WP_CORE_DIR} \
+      	--dbhost=${DB_HOST} \
+      	--dbname=${DB_NAME} \
+      	--dbuser=${DB_USER} \
+      	--dbpass=${DB_PASS}"
+
 # Install WordPress
-echo "Create WordPress config files"
-inc/bin/wp config create \
+echo "Create WordPress config files in ${WP_CORE_DIR}"
+"${home_dir}/inc/bin/wp" config create \
 	--path="${WP_CORE_DIR}" \
 	--dbhost="${DB_HOST}" \
 	--dbname="${DB_NAME}" \
 	--dbuser="${DB_USER}" \
 	--dbpass="${DB_PASS}"
 
-echo "Import database for WordPress"
-inc/bin/wp db import \
+echo "Install/Create the WordPress test site"
+"${home_dir}/inc/bin/wp" core install \
+	--title="Testing ${WP_ORG_PLUGIN_NAME}" \
+	--admin_user="admin" \
+	--admin_password="admin" \
+	--admin_email="thomas@eighty20results.com"
+
+echo "Return to home directory"
+cd "${home_dir}"
+
+# Set permissions
+chmod 644 "${WP_CORE_DIR}/wp-config.php"
+
+echo "Import database for WordPress to installation in ${WP_CORE_DIR}"
+"${home_dir}/inc/bin/wp" db import \
 	--path="${WP_CORE_DIR}" \
 	--dbuser="${DB_USER}" \
 	--dbpass="${DB_PASS}" \
 	--dbhost="${DB_HOST}" \
 	--dbname="${DB_NAME}" \
-	/.circleci/database/default_db.sql
-
-echo "Install/Create the WordPress test site"
-inc/bin/wp core install \
-	--path="${WP_CORE_DIR}" \
-	--url="http://${WP_HOST}" \
-	--title="${WP_ORG_PLUGIN_NAME} Tests" \
-	--admin_user="admin" \
-	--admin_password="admin" \
-	--admin_email="thomas@eighty20results.com"
+	"./.circleci/database/default_db.sql"
 
 echo "Setup rewrite rules for WordPress"
-inc/bin/wp rewrite structure \
+"${home_dir}/inc/bin/wp" rewrite structure \
 	--path="${WP_CORE_DIR}" \
 	'/%postname%/'
 
-echo "Copying the plugin sources to the correct directory"
 # Copy our plugin to WordPress directory
-cp -r ./ ${WP_CORE_DIR}/wp-content/plugins/${WP_ORG_PLUGIN_NAME}
+echo "Copying the plugin sources to the correct directory"
+cp -r "${home_dir}/*" "${WP_CORE_DIR}/wp-content/plugins/${WP_ORG_PLUGIN_NAME}"
 
 if [[ -n "${WP_DEPENDENCY_LIST}" ]]; then
-	echo "Install and activate dependencies"
-	inc/bin/wp plugin install --path="${WP_CORE_DIR}" "${WP_DEPENDENCY_LIST}" --activate
+	echo "Install and activate dependencies: ${WP_DEPENDENCY_LIST[*]}"
+	"${home_dir}/inc/bin/wp" plugin install --path="${WP_CORE_DIR}" "${WP_DEPENDENCY_LIST[*]}" --activate
 fi
 # Activate our plugin
 echo "Activating the plugin being tested"
-inc/bin/wp plugin activate --path="${WP_CORE_DIR}" ${WP_ORG_PLUGIN_NAME}
+"${home_dir}/inc/bin/wp" plugin activate --path="${WP_CORE_DIR}" "${WP_ORG_PLUGIN_NAME}"
